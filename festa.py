d0b4325623dead0e3e8daa869291ada09cbf45bd
matheusgbr01 <matheusgbr31@gmail.com>
2026-01-27 14:35:32 -0300

    Add files via upload

---
+++ festa.py
@@ -0,0 +1,78 @@
+
+
+'''Crie um programa que simule a lista de convidados de sua festa:
+Comece com uma lista contendo 10 convidados.
+
+O programa deve:
+
+a) Adicionar um novo convidado no final da lista
+
+b) Remover um convidado que você escolher
+
+c) Mostrar quantos convidados existem atualmente
+
+d) Mostrar o nome do primeiro e do último convidado da lista
+
+e) Alterar o nome do convidado que está na posição 1 (segundo da lista)
+'''
+
+
+
+nomes = ["joao" , "felipe" , "marcelo" , "victor" , "jetro" , "naldo", "ana" , "sofia" , "sara" , " vitória"]
+
+
+nomes.append ("tiago")
+print (nomes)
+
+nomes.remove ("joao")
+print (nomes)
+
+print(len(nomes))
+
+print(nomes[0])
+print(nomes [9])
+
+nomes[1] = ("estherfanny")
+
+print (nomes)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
